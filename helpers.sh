e8fc0c3f1f919fec7a01ff9b57011e414844b376
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2022-03-11 17:56:17 +0100

    Changed party example from Exercise_2 to planning a two-day conference.

--- helpers.sh
+++ helpers.sh
@@ -33,22 +33,22 @@ init_directory_with_empty_file () {
 }
 
 init_simple_repo () {
-    mkdir -p party_planning
-    cd party_planning
+    mkdir -p conference_planning
+    cd conference_planning
     git init
-    cp ../../../examples/flyer_A .
-    cp ../../../examples/flyer_B .
+    cp ../../../examples/schedule_day1 .
+    cp ../../../examples/schedule_day2 .
 
-    git add flyer_A && git commit -m "add flyer_A"
-    git add flyer_B && git commit -m "add flyer_B"
+    git add schedule_day1 && git commit -m "Add schedule_day1"
+    git add schedule_day2 && git commit -m "Add schedule_day2"
 
-    echo "Doors: 22:00" >> flyer_A
-    echo "Doors: 21:00" >> flyer_B
-    git add * && git commit -m "add opening time"
+    echo "09:00-11:00: Poster session" >> schedule_day1
+    echo "09:00-11:00: Poster session" >> schedule_day2
+    git add * && git commit -m "Add poster sessions in the morning"
 
-    echo "Happy Hour: 23:00 - 24:00" >> flyer_A
-    echo "Happy Hour: 22:00 - 24:00" >> flyer_B
-    git add * && git commit -m "add happy-hour"
+    echo "11:00-11:15: Coffee break" >> schedule_day1
+    echo "11:00-11:15: Coffee break" >> schedule_day2
+    git add * && git commit -m "Add coffee break"
 
     echo ""
     echo "Your commits so far:"
