4ec9063e0cfb804ada851ba16dbbc3c790313668
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2023-03-23 14:55:14 +0100

    Use .txt extensions for all files

--- beginner/helpers.sh
+++ beginner/helpers.sh
@@ -36,18 +36,18 @@ init_simple_repo () {
     mkdir -p conference_planning
     cd conference_planning
     git init
-    cp ../../../examples/schedule_day1 .
-    cp ../../../examples/schedule_day2 .
+    cp ../../../examples/schedule_day1.txt .
+    cp ../../../examples/schedule_day2.txt .
 
-    git add schedule_day1 && git commit -m "Add schedule_day1"
-    git add schedule_day2 && git commit -m "Add schedule_day2"
+    git add schedule_day1.txt && git commit -m "Add schedule_day1"
+    git add schedule_day2.txt && git commit -m "Add schedule_day2"
 
-    ed -s schedule_day1  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add poster sessions in the morning"
 
-    ed -s schedule_day1  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add coffee break"
 
     echo ""
@@ -65,26 +65,26 @@ init_simple_repo_remote () {
     mkdir -p conference_planning
     cd conference_planning
     git init
-    cp ../../../examples/schedule_day1 .
-    cp ../../../examples/schedule_day2 .
+    cp ../../../examples/schedule_day1.txt .
+    cp ../../../examples/schedule_day2.txt .
 
-    git add schedule_day1 && git commit -m "Add schedule_day1"
-    git add schedule_day2 && git commit -m "Add schedule_day2"
+    git add schedule_day1.txt && git commit -m "Add schedule_day1"
+    git add schedule_day2.txt && git commit -m "Add schedule_day2"
 
-    ed -s schedule_day1  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add poster sessions in the morning"
 
-    ed -s schedule_day1  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add coffee breaks"
 
     cd ..
     git clone conference_planning conference_planning_remote
     cd conference_planning_remote
     git switch -c "updated_schedules"
-    ed -s schedule_day1  <<< $'/break/\na\n11:15-12:15: Talk professor A.\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/break/\na\n11:15-12:15: Talk professor B.\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/break/\na\n11:15-12:15: Talk professor A.\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/break/\na\n11:15-12:15: Talk professor B.\n.\nw\nq' > /dev/null
     git add * && git commit -m "update schedules"
     git switch main
    
@@ -95,15 +95,15 @@ init_simple_repo_remote () {
 init_broken_repo () {
     init_simple_repo &> /dev/null
 
-    ed -s schedule_day1  <<< $'/break/\na\n11:15-12:15: Workshop ice crystal formation\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/break/\na\n11:15-12:12: Workshop secondary ice\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/break/\na\n11:15-12:15: Workshop ice crystal formation\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/break/\na\n11:15-12:12: Workshop secondary ice\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add workshops"
 
-    sed  's/Poster session/Talk professor C./g' schedule_day1 > schedule_day1_tmp
-    sed  's/Poster session/Talk professor D./g' schedule_day2 > schedule_day2_tmp
+    sed  's/Poster session/Talk professor C./g' schedule_day1.txt > schedule_day1_tmp.txt
+    sed  's/Poster session/Talk professor D./g' schedule_day2.txt > schedule_day2_tmp.txt
 
-    mv -f schedule_day1_tmp schedule_day1
-    mv -f schedule_day2_tmp schedule_day2
+    mv -f schedule_day1_tmp.txt schedule_day1.txt
+    mv -f schedule_day2_tmp.txt schedule_day2.txt
 
     git add * && git commit -m "Change poster sessions to talks"
 
@@ -122,17 +122,17 @@ init_repo_remote () {
     mkdir -p conference_planning
     cd conference_planning
     git init
-    cp ../../../examples/schedule_day1 .
+    cp ../../../examples/schedule_day1.txt .
 
-    git add schedule_day1 && git commit -m "Add schedule_day1"
+    git add schedule_day1.txt && git commit -m "Add schedule_day1"
 
-    ed -s schedule_day1  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add poster sessions in the morning"
 
-    ed -s schedule_day1  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add coffee breaks"
 
-    ed -s schedule_day1  <<< $'/break/\na\n11:15-12:15: Talk professor A.\n12:15-13:30: Lunch\n13:30-15:00: Workshop\n15:00-16:00: Talk professor B.\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/break/\na\n11:15-12:15: Talk professor A.\n12:15-13:30: Lunch\n13:30-15:00: Workshop\n15:00-16:00: Talk professor B.\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add rest of daily program"
 
     cd ..
@@ -148,8 +148,8 @@ init_repo_remote () {
 commit_to_remote_by_third_party() {
     cd ../conference_planning_remote
     git switch updated_schedules
-    sed  's/Poster session/Workshop/g' schedule_day1 > schedule_day1_tmp
-    mv -f schedule_day1_tmp schedule_day1
+    sed  's/Poster session/Workshop/g' schedule_day1.txt > schedule_day1_tmp.txt
+    mv -f schedule_day1_tmp.txt schedule_day1.txt
     git add * && git commit -m "Workshop in the morning"
     git switch main
     cd -
