894c0f2c30c140801151ca14c1dbd1241c55fed0
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2023-07-10 15:15:34 +0200

    Adapt file to deal with txt files

--- advanced/helpers.sh
+++ advanced/helpers.sh
@@ -6,18 +6,18 @@ init_advanced_repo () {
     mkdir -p conference_planning
     cd conference_planning
     git init
-    cp ../../git-course/advanced/examples/schedule_day1 .
-    cp ../../git-course/advanced/examples/schedule_day2 .
+    cp ../../git-course/advanced/examples/schedule_day1.txt .
+    cp ../../git-course/advanced/examples/schedule_day2.txt .
 
-    git add schedule_day1 && git commit -m "Add schedule_day1"
-    git add schedule_day2 && git commit -m "Add schedule_day2"
+    git add schedule_day1.txt && git commit -m "Add schedule_day1"
+    git add schedule_day2.txt && git commit -m "Add schedule_day2"
 
-    ed -s schedule_day1  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/program/\na\n09:00-11:00: Poster session\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add poster sessions in the morning"
 
-    ed -s schedule_day1  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
-    ed -s schedule_day2  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day1.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
+    ed -s schedule_day2.txt  <<< $'/session/\na\n11:00-11:15: Coffee break\n.\nw\nq' > /dev/null
     git add * && git commit -m "Add coffee break"
     git branch -m main
 
